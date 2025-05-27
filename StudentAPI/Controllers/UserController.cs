e417c7c1ece6da1dd59f18c2ea655ac7652cf92f
dana <perdanarobert9>
2025-05-27 20:35:27 +0700

    test

--- StudentAPI/Controllers/UserController.cs
+++ StudentAPI/Controllers/UserController.cs
@@ -10,38 +10,5 @@ namespace StudentAPI.Controllers
 {
     public class UserController : ControllerBase
     {
-        private readonly IUserRepository _userRepository;
-
-        public UserController(IUserRepository userRepository)
-        {
-            this._userRepository = userRepository;
-        }
-
-        [HttpPost("user")]
-        public async Task<ActionResult<User>> PostUser(User user)
-        {
-            var apiResult = new ApiResult<Object>();
-            try
-            {
-                var isExist = await _userRepository.UserIsExist(user.Name);
-                if (isExist)
-                {
-                    apiResult.Success = false;
-                    apiResult.Message = "The user has already exists!";
-                }
-                else
-                {
-                    _userRepository.Create(user);
-                    await _userRepository.SaveAsync();
-                }
-                return Ok(apiResult);
-            }
-            catch (Exception ex)
-            {
-                apiResult.Success = false;
-                apiResult.Message = ex.Message;
-                return StatusCode(500, apiResult);
-            }
-        }
     }
 }
